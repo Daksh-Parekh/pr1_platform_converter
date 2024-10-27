53c1572b812eb4da8ecb306c665d122d212ff5ce
Daksh-Parekh <dakshparekh42@gmail.com>
2024-10-27 07:18:54 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/pr1_platform_converter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pr1_platform_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isAndroid ? MaterialApp() : CupertinoApp();
+  }
+}
